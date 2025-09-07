fd45c2933f1dcde6b042f1ee03e50a805f1bd36d
Ray Chen <ray@raychen.io>
2025-09-08 07:25:59 +0800

    .

--- server.sh
+++ server.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-while true; do 
-    nc -l -p 5432 -q 1 <<< ""
-    sleep $((2 + RANDOM % 3))
+while true; do
+  nc -l -p 5432 &
+  NC_PID=$!
+  sleep 0.1
+  kill -9 $NC_PID 2>/dev/null
+  sleep $((2 + RANDOM % 3))
 done
