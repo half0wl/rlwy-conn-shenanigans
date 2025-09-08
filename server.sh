fd1a141dfac6618db88b5fb9763e7f32c4eca85c
Ray Chen <ray@raychen.io>
2025-09-08 08:17:40 +0800

    .

--- server.sh
+++ server.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 while true; do
-  nc -l -p 5432 &
-  NC_PID=$!
-  kill -9 $NC_PID 2>/dev/null
+  (
+    echo -ne '\x52\x00\x00\x00\x08\x00\x00\x00\x00' | nc -l -p 5432 -q 0 &
+    NC_PID=$!
+    sleep 0.5
+    kill -9 $NC_PID 2>/dev/null # rst
+  )
+  sleep 1
 done
