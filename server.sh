f84e6cd840473590da55e6630d8a3fecd87d8e63
Ray Chen <ray@raychen.io>
2025-09-08 08:28:02 +0800

    .

--- server.sh
+++ server.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
 while true; do
-  PIPE=$(mktemp -u)
-  mkfifo $PIPE
+  (
+    {
+      dd bs=1 count=100 2>/dev/null
 
-  nc -l -p 5432 <$PIPE | (
-    read -n 8
+      # pg handshake proto
+      # 1. AuthenticationOk (R)
+      printf '\x52\x00\x00\x00\x08\x00\x00\x00\x00'
 
-    # pg proto
-    # 1. AuthenticationOk (R)
-    echo -ne '\x52\x00\x00\x00\x08\x00\x00\x00\x00'
+      # 2. ParameterStatus - server_version
+      printf '\x53\x00\x00\x00\x1a\x73\x65\x72\x76\x65\x72\x5f\x76\x65\x72\x73\x69\x6f\x6e\x00\x31\x34\x2e\x30\x00'
 
-    # 2. ParameterStatus messages (S) server_version
-    echo -ne '\x53\x00\x00\x00\x1a\x73\x65\x72\x76\x65\x72\x5f\x76\x65\x72\x73\x69\x6f\x6e\x00\x31\x34\x2e\x30\x00'
+      # 3. BackendKeyData (K)
+      printf '\x4b\x00\x00\x00\x0c\x00\x00\x00\x01\x00\x00\x00\x02'
 
-    # 3. BackendKeyData (K)
-    echo -ne '\x4b\x00\x00\x00\x0c\x00\x00\x00\x01\x00\x00\x00\x02'
+      # 4. ReadyForQuery (Z) - Idle transaction state
+      printf '\x5a\x00\x00\x00\x05\x49'
 
-    # 4. ReadyForQuery (Z) ready for queries
-    echo -ne '\x5a\x00\x00\x00\x05\x49'
+      sleep 2
+    } | nc -l -p 5432 -q 0
+  ) &
 
-    # keepalive
-    sleep 2
-  ) >$PIPE &
+  PID=$!
 
-  PROCESS_PID=$!
-  wait $PROCESS_PID
-  pkill -9 -P $$ nc 2>/dev/null # rst it
-  rm -f $PIPE
+  sleep 2.1
+  kill -9 $PID 2>/dev/null # RST
+
   sleep 1
 done
